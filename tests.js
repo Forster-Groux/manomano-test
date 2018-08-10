f00f5c253893a00968a5d6f4d7e58fb5e4494d72
Forster Groux <forster.groux-sc@airliquide.com>
2018-08-10 16:33:25 +0200

    add error management

--- tests.js
+++ tests.js
@@ -9,3 +9,13 @@ let expected2 = getOccurrenceNumber(fixtures.text2, 'mano');
 
 assert.equal(expected1, 4, `Expected 4 occurrences got ${expected1}`);
 assert.equal(expected2, 0, `Expected 0 occurrence got ${expected2}`);
+
+assert.throws(() => getOccurrenceNumber(null, 'mano'), Error, 'Expected "invalid text" error');
+assert.throws(() => getOccurrenceNumber('', 'mano'), Error, 'Expected "invalid text" error');
+assert.throws(() => getOccurrenceNumber('a', 'mano'), Error, 'Expected "invalid text" error');
+
+assert.throws(() => getOccurrenceNumber(fixtures.text2), Error, 'Expected "invalid word" error');
+assert.throws(() => getOccurrenceNumber(fixtures.text2, ''), Error, 'Expected "invalid word" error');
+assert.throws(() => getOccurrenceNumber(fixtures.text2, 'b'), Error, 'Expected "invalid word" error');
+
+console.log("All tests passed successfully");
